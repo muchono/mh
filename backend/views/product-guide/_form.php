89123572d506ee4e7605479924d8d9c1fa39ec9f
Valeriy Ermolin <vermolin@geeksforless.net>
2017-05-16 11:34:55 +0300

    +

--- backend/views/product-guide/_form.php
+++ backend/views/product-guide/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\tinymce\TinyMce;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\ProductGuide */
@@ -18,7 +19,18 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'status')->hiddenInput(['maxlength' => true, 'value' => 1])->label(false); ?>
 
-    <?= $form->field($model, 'about')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'about')->widget(TinyMce::className(), [
+    'options' => ['rows' => 26],
+    'language' => 'en_GB',
+    'clientOptions' => [
+        'plugins' => [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste "
+        ],
+        'toolbar' => "undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image "
+    ]
+    ]);?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
